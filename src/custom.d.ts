d2e5d53b3aaa5d4828a7814a714da925e72dfe79
isaSyr <lisabenamisa@yahoo.com>
2024-12-16 22:58:01 +0300

    -switched to webpack

---
+++ src/custom.d.ts
@@ -0,0 +1,11 @@
+// src/custom.d.ts
+declare module '*.svg' {
+    const content: string;
+    export default content;
+  }
+  
+  declare module '*.css' {
+    const content: string;
+    export default content;
+  }
+  
